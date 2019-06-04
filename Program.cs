6f0e0737ab102e10b1630aa11de2969f7a5dd16b
peter sigurdson <peter@petersigurdson.net>
2019-06-03 21:59:06 -0400

    level 1

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the number's factors.
+           
+            long num = 342345;
+            List<long> factors = FindFactors(num);
+
+            // Display the factors.
+            List<string> strings = factors.ConvertAll(x => x.ToString());
+            txtFactors.Text = string.Join(" x ", strings.ToArray());
+        }
+
+        private static List<long> FindFactors(long num)
+        {
+            List<long> result = new List<long>();
+
+            // Take out the 2s.
+            while (num % 2 == 0)
+            {
+                result.Add(2);
+                num /= 2;
+            }
+
+            // Take out other primes.
+            long factor = 3;
+            while (factor * factor <= num)
+            {
+                if (num % factor == 0)
+                {
+                    // This is a factor.
+                    result.Add(factor);
+                    num /= factor;
+                }
+                else
+                {
+                    // Go to the next odd number.
+                    factor += 2;
+                }
+            }
+
+            // If num is not 1, then whatever is left is prime.
+            if (num > 1) result.Add(num);
+
+            return result;
+        }
+    }
+}
